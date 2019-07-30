1888d06f48f37657cec79bcf99ced3cefc2bdb6b
tgmattso <timothy.g.mattson@intel.com>
2019-07-30 13:26:36 -0700

    fixed racey so it is just the serial code

--- OMP_Exercises/racey.c
+++ OMP_Exercises/racey.c
@@ -7,19 +7,10 @@ int main()
 
   printf("I think");
 
-omp_set_num_threads(3);
-#pragma omp parallel
-{
-  #pragma omp single
-  {
-  #pragma omp task
   printf(" car");
 
-  #pragma omp task
   printf(" race");
-  }
 
-}
   printf("s are fun\n");
 
 }
